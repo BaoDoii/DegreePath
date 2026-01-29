d0afd7a4f3059702c480a08e4833538c40c06e30
BaoDoii <BRIANHA29@GMAIL.COM>
2026-01-29 02:36:46 -0800

    added backend Spring Boot merge

---
+++ backend/src/main/java/com/degreepath/degreepath/DegreepathApplication.java
@@ -0,0 +1,13 @@
+package com.degreepath.degreepath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DegreepathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DegreepathApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/degreepath/degreepath/DegreepathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.degreepath.degreepath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DegreepathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
