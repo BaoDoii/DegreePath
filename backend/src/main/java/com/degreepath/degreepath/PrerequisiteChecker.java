54a8aba8caa99d634930545f6fec29b5e4ea40cd
BaoDoii <BRIANHA29@GMAIL.COM>
2026-01-30 00:34:27 -0800

    Update PrerequisiteChecker.java

--- backend/src/main/java/com/degreepath/degreepath/PrerequisiteChecker.java
+++ backend/src/main/java/com/degreepath/degreepath/PrerequisiteChecker.java
@@ -3,5 +3,7 @@ package com.degreepath.degreepath;
 import java.util.List;
 
 public class PrerequisiteChecker {
-
+	public static boolean canTakeCourse(Course course, List<String> completedCourses) {
+		return false;
+	}
 }
