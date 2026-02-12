fec6b7c1174da1a26035b887ab37f3254c2bca6b
BaoDoii <BRIANHA29@GMAIL.COM>
2026-02-12 01:29:10 -0800

    Update SemesterPlanner.java

--- backend/src/main/java/com/degreepath/degreepath/SemesterPlanner.java
+++ backend/src/main/java/com/degreepath/degreepath/SemesterPlanner.java
@@ -5,29 +5,6 @@ import java.util.ArrayList;
 
 public class SemesterPlanner {
 	
-	public static List<Course> generateSemester(List<String> completedCourses, List<Course> allCourses, int maxUnits){
-		List<Course> selectedCourses = new ArrayList<>();
-		int totalUnits = 0;
-		
-		//can take courses, not guided yet but just fixed to add up to max Units for now v1
-		for(Course course: allCourses) {
-			
-			if(completedCourses.contains(course.getCode())) {
-				continue;
-			}
-			
-			boolean canTake = PrerequisiteChecker.canTakeCourse(course, completedCourses);
-			if(canTake) {
-				if(totalUnits + course.getUnits() <= maxUnits) {
-				selectedCourses.add(course);
-				totalUnits += course.getUnits();
-				}
-			}
-		}
-		
-		return selectedCourses;
-	}
-	
 	//count how many courses depend on the course as a prereq
 	private static int countDependents(Course course, List<Course> allCourses) {
 		int count = 0;
@@ -38,6 +15,45 @@ public class SemesterPlanner {
 			}
 		}
 		return count;
+	}
+	
+	public static List<Course> generateSemester(List<String> completedCourses, List<Course> allCourses, int maxUnits){
+		List<Course> selectedCourses = new ArrayList<>();
+		List<Course> availableCourses = new ArrayList<>();
+		int totalUnits = 0;
+		
+		//collect available courses 
+		for(Course c : allCourses) {
+			if(completedCourses.contains(c.getCode())) {
+				continue;
+			}
+			
+			boolean canTake = PrerequisiteChecker.canTakeCourse(c, completedCourses);
+			if(canTake) {
+				availableCourses.add(c);
+			}
+			
+		}
+		
+		//sort by priority
+		availableCourses.sort((a,b) -> {
+			int aCount = countDependents(a, allCourses);
+			int bCount = countDependents(b, allCourses);
+			return Integer.compare(bCount,aCount);
+		});
+		
+		
+		
+		//can take courses, v2  
+		for(Course course: availableCourses) {
+			if(totalUnits + course.getUnits() <= maxUnits) {
+				selectedCourses.add(course);
+				totalUnits += course.getUnits();
+			}
+			
+		}
+		
+		return selectedCourses;
 	}
 	
 }
